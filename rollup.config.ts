626075ad2f98d556d70af247c0bb17e10c86e828
Anej Gorkič <anej.gorkic@gmail.com>
2021-11-28 12:23:44 +0100

    Add d.ts types generation to single file

--- rollup.config.ts
+++ rollup.config.ts
@@ -2,42 +2,50 @@ import pkg from "./package.json";
 import external from "rollup-plugin-peer-deps-external";
 import resolve from "rollup-plugin-node-resolve";
 import esbuild from "rollup-plugin-esbuild";
+import dts from "rollup-plugin-dts";
 
-export default {
-  input: "src/index.ts",
-  output: [
-    {
-      file: pkg.main,
-      format: "cjs",
-      exports: "named",
-      sourcemap: true,
-    },
-    {
-      file: pkg.module,
-      format: "es",
-      exports: "named",
-      sourcemap: true,
-    },
-  ],
-  plugins: [
-    external(),
-    resolve(),
-    esbuild({
-      // All options are optional
-      include: /\.[jt]sx?$/, // default, inferred from `loaders` option
-      exclude: /node_modules/, // default
-      sourceMap: false, // default
-      minify: false,
-      target: "es2017", // default, or 'es20XX', 'esnext'
-      jsx: "transform", // default, or 'preserve'
-      jsxFactory: "React.createElement",
-      jsxFragment: "React.Fragment",
-      // Like @rollup/plugin-replace
-      define: {
-        __VERSION__: '"x.y.z"',
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: pkg.main,
+        format: "cjs",
+        exports: "named",
+        sourcemap: true,
       },
-      tsconfig: "tsconfig.json",
-    }),
-  ],
-  external: ["react", "react-dom"],
-};
+      {
+        file: pkg.module,
+        format: "es",
+        exports: "named",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      external(),
+      resolve(),
+      esbuild({
+        // All options are optional
+        include: /\.[jt]sx?$/, // default, inferred from `loaders` option
+        exclude: /node_modules/, // default
+        sourceMap: true, // default
+        minify: true,
+        target: "es2017", // default, or 'es20XX', 'esnext'
+        jsx: "transform", // default, or 'preserve'
+        jsxFactory: "React.createElement",
+        jsxFragment: "React.Fragment",
+        // Like @rollup/plugin-replace
+        define: {
+          __VERSION__: '"x.y.z"',
+        },
+        tsconfig: "tsconfig.json",
+      }),
+    ],
+    external: ["react", "react-dom"],
+  },
+  {
+    input: "./dist/dts/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "es" }],
+    plugins: [dts()],
+  },
+];
